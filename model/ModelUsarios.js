d758d2d093d7d269256c3b9ed3b7c347232fcf0a
CristianMaygo <maygo782@gmail.com>
2024-10-07 12:28:31 -0500

    impletacion de modulos y aplicaciones

---
+++ model/ModelUsarios.js
@@ -0,0 +1,13 @@
+export let Usuario = [
+    {
+        usario: "admin",
+        contraseña: "1234567"
+    },
+    {
+        usario: "superAdmin",
+        contraseña: "25604"
+    }
+
+]
+
+/* Objectos */
--- view/public/js/script.js
+++ view/public/js/script.js
@@ -11,10 +11,41 @@ querySelector
 
 
 
-console.log(document.getElementById("Usuario")) /* Solo muestra un elemento */
-console.log(document.getElementsByClassName("form")) /* Siempre muestra un arreglo */
-console.log(document.getElementsByTagName("input")) /* Siempre muestra un arreglo */
+
+/* console.log(document.getElementById("Usuario")) Solo muestra un elemento 
+console.log(document.getElementsByClassName("form")) Siempre muestra un arreglo 
+console.log(document.getElementsByTagName("input"))  Siempre muestra un arreglo 
 
 
 console.log(document.querySelector("form"))
-console.log(document.querySelectorAll("form"))
+console.log(document.querySelectorAll("form")) */
+
+
+import { Usuario } from "../../../model/ModelUsarios.js"
+
+console.log(Usuario)
+
+function iniciarSesion(){ //Función declarativa
+    let Usuario = document.getElementById("Usuario").value
+    let Contraseña = document.getElementById("Contraseña").value
+    if(Usuario == 'admin' && Contraseña == '12345'){
+        console.log('Bienvenido')
+    } else {
+        console.log('Error de credenciales')
+    }
+}
+
+
+
+/* let iniciarSesion = function(){ Función de expresión 
+    let Usuario = document.getElementById("Usuario");
+    let Contraseña = document.getElementById("Contraseña");
+    console.log(Usuario)
+    console.log(Contraseña)
+    console.log('Iniciar Sesión'); 
+}
+iniciarSesion() */
+
+
+/* console.log(Contraseña)
+console.log(Usuario); */
